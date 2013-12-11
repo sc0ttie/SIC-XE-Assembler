2c149f5b6db1244298e66be6278bc3756c0eccdb
Malthael <ys111110111@gmail.com>
2013-12-11 15:20:28 +0800

    Add new class Operation for op table

---
+++ src/assembler/Operation.java
@@ -0,0 +1,40 @@
+package assembler;
+
+/**
+ *
+ * @author Malthael
+ */
+public class Operation {
+    private String _mnemonic;
+    private String _opcode;
+    private int _format;
+    
+    public Operation(String mnemonic, String opcode, String format) {
+        _mnemonic = mnemonic;
+        _opcode = opcode;
+        
+        switch (format) {
+            case "1":
+                _format = 1;
+                break;
+            case "2":
+                _format = 2;
+                break;
+            case "3/4":
+                _format = 3;
+                break;
+        }
+    }
+    
+    public String mnemonic() {
+        return _mnemonic;
+    }
+    
+    public String opcode() {
+        return _opcode;
+    }
+    
+    public int format() {
+        return _format;
+    }
+}
